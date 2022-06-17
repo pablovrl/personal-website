e1f9c7ec94fa5d930b17b2261621b15cbf298ffa
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-06-16 20:18:37 -0400

    change portfolio view

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,6 +1,5 @@
-import { Link, Box, Flex, HStack, useColorModeValue } from "@chakra-ui/react";
-import { AiFillGithub } from "react-icons/ai";
-import { BiLinkExternal } from "react-icons/bi";
+import { Box, Flex, HStack, useColorModeValue } from "@chakra-ui/react";
+import Image from "next/image";
 
 interface ProjectProps {
   image: string;
@@ -8,6 +7,7 @@ interface ProjectProps {
   githubUrl: string;
   children?: React.ReactNode;
   iconColor: "white" | "black";
+  margin: boolean;
 }
 
 interface IconLinkProps {
@@ -15,57 +15,23 @@ interface IconLinkProps {
   children: React.ReactNode;
 }
 
-const IconLink = ({ href, children }: IconLinkProps) => (
-  <Link href={href} isExternal>
-    <Box
-      _hover={{ transform: "scale(1.2)" }}
-      css={{ transition: "transform 180ms ease-in-out" }}
-    >
-      {children}
-    </Box>
-  </Link>
-);
-
-const Project = ({
-  image,
-  pageUrl,
-  githubUrl,
-  iconColor,
-  children,
-}: ProjectProps) => {
+const Project = ({ image, children, margin }: ProjectProps) => {
   return (
     <Box>
-      <Box
-        border={"2px"}
-        borderColor={useColorModeValue("#728D88", "#C06E70")}
-        bgSize={"cover"}
-        bgImage={image}
-        bgPos={"center"}
+      <Flex
         rounded="2xl"
+        mt={{ md: margin ? "16" : 0 }}
+        backgroundColor="gray.100"
       >
-        <Flex
-          alignItems={"center"}
-          justifyContent={"center"}
-          h="180px"
-          opacity={0}
-          _hover={{ opacity: 1 }}
-          rounded="2xl"
-          css={{
-            backdropFilter: "blur(5px)",
-          }}
-        >
-          <HStack spacing={8}>
-            {pageUrl && (
-              <IconLink href={pageUrl}>
-                <BiLinkExternal color={iconColor} size={40} />
-              </IconLink>
-            )}
-            <IconLink href={githubUrl}>
-              <AiFillGithub color={iconColor} size={40} />
-            </IconLink>
-          </HStack>
-        </Flex>
-      </Box>
+        <Image
+          objectFit="cover"
+          src={image}
+          alt="project image"
+          width={1600}
+          height={1000}
+          style={{borderRadius: "8px"}}
+        />
+      </Flex>
       {children}
     </Box>
   );
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,6 +1,16 @@
-import { Box, Heading, SimpleGrid, SlideFade, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  Link,
+  SimpleGrid,
+  SlideFade,
+  Text,
+} from "@chakra-ui/react";
 import { NextPage } from "next";
 import Project from "../components/Project";
+import { AiFillGithub } from "react-icons/ai";
+import { BiLinkExternal } from "react-icons/bi";
 
 interface Project {
   image: string;
@@ -48,29 +58,61 @@ const projects: Project[] = [
   },
 ];
 
+const ProjectLink = ({
+  href,
+  children,
+  icon,
+}: {
+  href: string;
+  children: React.ReactNode;
+  icon: React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+}) => (
+  <Link href={href} isExternal _hover={{ textDecoration: "none" }}>
+    <Button
+      bgColor="#313134"
+      color="#EDEDEE"
+      _hover={{ backgroundColor: "#444446" }}
+      leftIcon={icon}
+    >
+      {children}
+    </Button>
+  </Link>
+);
+
 const Portfolio: NextPage = () => {
   return (
     <Box pt={8}>
       <SlideFade in>
-        <SimpleGrid
-          mx={{ base: 8, md: 14 }}
-          gap={8}
-          columns={{ base: 1, md: 2 }}
-        >
-          {projects.map((project) => (
+        <SimpleGrid gap={10} columns={{ base: 1, md: 2 }}>
+          {projects.map((project, i) => (
             <Project
               key={project.githubUrl}
               githubUrl={project.githubUrl}
               image={project.image}
               pageUrl={project.pageUrl}
               iconColor={project.iconColor}
+              margin={i % 2 !== 0}
             >
-              <Text mt={2} textAlign="center" fontSize={"xl"}>
+              <Text mt={2} fontSize={"xl"}>
                 {project.title}
               </Text>
-              <Text fontSize={"sm"} textAlign={"center"}>
-                {project.description}
-              </Text>
+              <Text fontSize={"sm"}>{project.description}</Text>
+              <HStack mt={2}>
+                <ProjectLink
+                  icon={<AiFillGithub size={20} />}
+                  href={project.githubUrl}
+                >
+                  Código
+                </ProjectLink>
+                {project.pageUrl && (
+                  <ProjectLink
+                    icon={<BiLinkExternal size={20} />}
+                    href={project.pageUrl}
+                  >
+                    Deploy
+                  </ProjectLink>
+                )}
+              </HStack>
             </Project>
           ))}
         </SimpleGrid>
