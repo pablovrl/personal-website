addf8d9235e0c74b341c4d09f8cf46e0ed48963f
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-09-18 18:20:27 -0300

    move devicons cdn

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -16,10 +16,6 @@ const Main = ({ children }: { children: React.ReactElement }) => {
         <title>Pablo Villarroel</title>
         <link rel="icon" type="image/svg+xml" href="planet.svg" />
 
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
-        />
       </Head>
       <Navbar />
       <Container maxW={"container.md"} pt={"20"}>
