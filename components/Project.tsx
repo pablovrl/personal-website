d580abea85db05845726908fd99534a84c78a1eb
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-09-27 19:43:57 -0300

    add card to project

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, HStack, Link, useColorModeValue } from "@chakra-ui/react";
+import { Box, color, Flex, Link, useColorMode } from "@chakra-ui/react";
 import Image from "next/image";
 
 interface ProjectProps {
@@ -10,35 +10,35 @@ interface ProjectProps {
   deployLink?: string;
 }
 
-interface IconLinkProps {
-  href: string;
-  children: React.ReactNode;
-}
-
 const Project = ({ image, children, deployLink }: ProjectProps) => {
+  const { colorMode } = useColorMode();
+  const borderColor = colorMode === "light" ? "gray.200" : "gray.700";
   return (
-    <Box>
+    <Box border="1px" borderColor={borderColor} borderRadius={"xl"}>
       <Link
         href={deployLink}
         isExternal
         cursor={deployLink ? "pointer" : "default"}
         _focus={{ border: "0px" }}
       >
-        <Flex
-          rounded="2xl"
-          backgroundColor="gray.100"
-        >
+        <Flex rounded="2xl" backgroundColor="gray.100">
           <Image
             objectFit="cover"
             src={image}
             alt="project image"
             width={1600}
             height={1000}
-            style={{ borderRadius: "8px" }}
+            style={{ borderRadius: "0.75rem 0.75rem 0rem 0rem" }}
           />
         </Flex>
       </Link>
-      {children}
+      <Box
+        p={{ md: 5 }}
+        borderTop="1px"
+        borderColor={borderColor}
+      >
+        {children}
+      </Box>
     </Box>
   );
 };
